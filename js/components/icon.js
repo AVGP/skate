d5ad615f8916cd483afbd1847649261ae5e18dbb
Trey Shugart <treshugart@gmail.com>
2014-04-28 18:14:48 +1000

    Fix external link and icon components.

--- js/components/icon.js
+++ js/components/icon.js
@@ -1,5 +1,5 @@
 define(['skate'], function(skate) {
-  return skate('skate-template', {
+  return skate('skate-icon', {
     restrict: '[skate-icon]',
     insert: function(element) {
       element.className = 'glyphicon glyphicon-' + element.getAttribute('skate-icon');
